1b4f695d16477d31373d79c99c50174285d98ba5
mcwilk <mcwilk.dev@gmail.com>
2025-12-15 11:32:25 +0100

    Add validation for variables

--- variables.tf
+++ variables.tf
@@ -6,13 +6,21 @@ variable "subscription_id" {
 variable "rg_name" {
   description = "The name of the Resource Group"
   type        = string
-  default     = "HelloWorld_RG_TF"
+  default     = "HelloWorld_RG_TFabc"
+  validation {
+    condition     = length(var.rg_name) > 10 && endswith(upper(var.rg_name), "RG_TF")
+    error_message = "The resource group name must longer than 10 characters and end with RG_TF"
+  }
 }
 
 variable "location" {
   description = "The Azure location where resources will be created"
   type        = string
   default     = "westus"
+  validation  {
+    condition     = contains(["eastus", "westus", "centralus"], lower(var.location))
+    error_message = "Location must be one of: eastus, westus, centralus"
+  }
 }
 
 variable "vnet_address_space" {
