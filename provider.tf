0a02219c44727a0c3aa5e16cbb819c790faca4a3
mcwilk <mcwilk.dev@gmail.com>
2025-12-15 16:14:23 +0100

    Add alias to AZ provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   resource_provider_registrations = "none"
   subscription_id                 = var.subscription_id
+  # alias                           = "main_az"
   features {}
 }
